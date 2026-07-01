3b29c04a98a85e2f46370fa7cdb34bea62edc93a
Tom Jau <jellopancake213@gmail.com>
2026-07-01 10:30:09 +0800

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import jest from 'eslint-plugin-jest';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  js.configs.recommended,
+  ...compat.extends("eslint-config-airbnb-base"), 
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module", // This fixes the "Unexpected token import" error
+      ecmaVersion: "latest",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+      "import/extensions": "off", // Fixes the "Unexpected use of file extension" error
+      "import/no-extraneous-dependencies": "off",
+      "no-underscore-dangle": ["error", { "allow": ["__filename", "__dirname"] }],
+    },
+  },
+  {
+    files: ['**/tests/**/*.js', '**/*.test.js'], // Adjust this pattern to match your test files
+    plugins: {
+      jest: jest,
+    },
+    rules: {
+      ...jest.configs.recommended.rules,
+    },
+  },
+  eslintConfigPrettier, 
+  
+];
